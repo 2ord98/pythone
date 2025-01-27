5267a94c4a73f7909545fb43baa46e85951bae4e
2ord <lorenzo.cassiani@gmail.com>
2025-01-27 10:50:45 +0100

    Update media.py

--- media.py
+++ media.py
@@ -1,19 +1,18 @@
-risposta = (input("ciao! hai un numero da darmi? se si, scrivi 'True' altrimenti 'False':  "))
-numero =0
+#rifatto per il prof
 totale_numeri = 0
 totale_cicli = 0
+numero = "poldo"  # Inizializza con un valore che non è "fine" per iniziare il ciclo
 
-if risposta == "True" :
-    #ciclowhile
-    risposta_ciclo = "True"
-    while risposta_ciclo == "True":
-     numero = int(input("ehi dammi pure un numero: "))
-     totale_numeri = numero + totale_numeri
-     risposta_ciclo = str(input("hai un altro numero da darmi?:  "))
-     totale_cicli = 1+ totale_cicli
-else :
- totale_numeri= 0
- totale_cicli = 1
+# Ciclo while
+while numero != "fine":
+    numero = input("Ehi, dammi pure un numero (o 'fine' per terminare): ")
 
+    if numero == "fine":
+        break  # Esce dal ciclo quando l'utente scrive "fine"
+    
+    else :
+        totale_numeri += int(numero)  # Converte l'input in un numero intero e aggiunge al totale
+        totale_cicli += 1  # Incrementa il conteggio dei numeri inseriti
+    
 print ("la media dei tuoi numeri è " , totale_numeri/totale_cicli )
 print (" grazie e arrivederci")
