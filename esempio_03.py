b670752de3a024324d5af9d9266295bb070dfe6e
2ord <lorenzo.cassiani@gmail.com>
2025-02-11 06:08:47 +0100

    Add files via upload

---
+++ esempio_03.py
@@ -0,0 +1,57 @@
+#esercizio partita di tennis Sinner vs Alcaraz
+punteggio_uno = 0
+punteggio_due = 0
+
+while punteggio_uno <= 40 and punteggio_due <= 40 :
+    scambio= int(input("Stai tenendo il punteggio del match di tennis. Inserisci il numero 1, se vince lo scambio Sinner, 2 per Alcaraz: "))
+    if scambio == 1 and punteggio_uno < 30 :
+        punteggio_uno += 15
+        print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
+    elif scambio == 1 and punteggio_uno == 30:
+        punteggio_uno += 10
+        print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
+    elif scambio == 1 and punteggio_uno == 40:
+        print ("Vince il game Sinner")
+        break   
+    elif scambio == 2 and punteggio_due < 30 :
+        punteggio_due += 15
+        print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
+    elif scambio == 2 and punteggio_due == 30:
+        punteggio_due += 10
+        print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
+    elif scambio == 2 and punteggio_due == 40:
+        print ("Vince il game Alcaraz")
+        break 
+    else: 
+        print("Errore, inserisci un valore corretto")
+    if punteggio_uno == 40 and punteggio_due == 40 :
+        break
+
+#siamo arrivati al tie break o 'vantaggi'
+if punteggio_uno == punteggio_due == 40 :  
+    print("Siamo al tie break ")
+    print (punteggio_uno , "-" , punteggio_due)
+    while True :
+        punto_finale = int(input("Inserisci il numero 1, se vince lo scambio dei Vantaggi, Sinner; 2 per Alcaraz: "))
+        if punto_finale == 1 and punteggio_uno == 40 :
+           punteggio_uno = "VANT"
+           print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
+        elif punto_finale == 1 and punteggio_uno == "VANT" and punteggio_due == "VANT":
+           punteggio_uno = "VANT"
+           punteggio_due = 40
+           print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
+        elif punto_finale == 1 and punteggio_uno == "VANT" :
+           print ("Vince il game Sinner")
+           break   
+        elif punto_finale == 2 and punteggio_due == 40 :
+           punteggio_due = "VANT"
+           print("Punteggio:", punteggio_uno , "-" , punteggio_due )
+        elif punto_finale == 2 and punteggio_uno == "VANT" and punteggio_due == "VANT":
+           punteggio_uno = 40
+           punteggio_due = "VANT"
+           print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
+        else:
+            print ("Vince il game Alcaraz")
+            break
+else:
+    print("Prossimo Game")
