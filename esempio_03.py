666901ed85dffbcdd2a5ed319f32ec97bc5c72ef
2ord <lorenzo.cassiani@gmail.com>
2025-02-11 07:39:10 +0100

    Update esempio_03.py

--- esempio_03.py
+++ esempio_03.py
@@ -1,4 +1,4 @@
-#esercizio partita di tennis Sinner vs Alcaraz
+#esercizio partita di tennis Sinner(1) vs Alcaraz(2)
 punteggio_uno = 0
 punteggio_due = 0
 
@@ -30,23 +30,22 @@ while punteggio_uno <= 40 and punteggio_due <= 40 :
 #siamo arrivati al tie break o 'vantaggi'
 if punteggio_uno == punteggio_due == 40 :  
     print("Siamo al tie break ")
-    print (punteggio_uno , "-" , punteggio_due)
     while True :
-        punto_finale = int(input("Inserisci il numero 1, se vince lo scambio dei Vantaggi, Sinner; 2 per Alcaraz: "))
-        if punto_finale == 1 and punteggio_uno == 40 :
+        scambio = int(input("Inserisci il numero 1, se vince lo scambio dei Vantaggi, Sinner; 2 per Alcaraz: "))
+        if scambio == 1 and punteggio_uno == 40 :
            punteggio_uno = "VANT"
            print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
-        elif punto_finale == 1 and punteggio_uno == "VANT" and punteggio_due == "VANT":
+        elif scambio == 1 and punteggio_uno == "VANT" and punteggio_due == "VANT":
            punteggio_uno = "VANT"
            punteggio_due = 40
            print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
-        elif punto_finale == 1 and punteggio_uno == "VANT" :
+        elif scambio == 1 and punteggio_uno == "VANT" :
            print ("Vince il game Sinner")
            break   
-        elif punto_finale == 2 and punteggio_due == 40 :
+        elif scambio == 2 and punteggio_due == 40 :
            punteggio_due = "VANT"
            print("Punteggio:", punteggio_uno , "-" , punteggio_due )
-        elif punto_finale == 2 and punteggio_uno == "VANT" and punteggio_due == "VANT":
+        elif scambio == 2 and punteggio_uno == "VANT" and punteggio_due == "VANT":
            punteggio_uno = 40
            punteggio_due = "VANT"
            print("Punteggio:" , punteggio_uno , "-" , punteggio_due)
