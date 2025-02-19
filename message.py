e1aa1e83e602e4d4a66605612b63895b7ceb0ae4
Lorenzo Cassiani <lorenzo.cassiani@gmail.com>
2025-02-19 10:32:31 +0100

    Update message.py

--- message.py
+++ message.py
@@ -1,21 +1,22 @@
+#esercizio corretto
 class Message:
-    def __init__ (self, destinatario = "", mittente = "", messaggio= ""):
-        self._destinatario = destinatario
-        self._mittente = mittente
-        self._messaggio = messaggio
+    def __init__ (self, destinatario, mittente):
+      self._destinatario = destinatario
+      self._mittente = mittente
+      self._messaggio = [] #oppure self._messaggio = []
     def destinatario(self) :
-     return self._destinatario
+      return self._destinatario
     def mittente(self) :
-     return self._mittente
+      return self._mittente
     def messaggio(self):
-     return self._messaggio
-    def append_riga(self, riga =""):
-      self._messaggio += (" " + riga) if self._messaggio else riga   #Append riga
-    def toString_riga(self):
-      return (f"Da: {self._mittente} \n A: {self._destinatario} \n Messaggio: \n {self._messaggio}")
-      
+      return self._messaggio
+    def append(self, riga):
+      self._messaggio.append(riga) #Append riga oppure questo metodo
+    def toString(self):
+      messaggio_formattato = " ".join(self._messaggio)
+      print (f"Da:{self._mittente}\nA:{self._destinatario}\nMessaggio:\n{messaggio_formattato}")
 
-mess = Message("riccardo", "lorenzo" , "Ciao! riccardo sono Lorenzo e ti scrivo oggi,")
-mess.append_riga("come stai?")
-mess.append_riga("e sei andato ieri a bambini?")
-print(mess.toString_riga())
+mess = Message("riccardo", "lorenzo")
+mess.append("Ciao, come stai?")
+mess.append("e sei andato ieri a funghi?")
+mess.toString()
